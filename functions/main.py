6646fa5bc2decd5dd1b7127f0413c0f7aa0dfa72
Rudy Ayitinya <ayitinya@outlook.com>
2024-02-16 08:05:22 +0000

    sets up firebase functions

---
+++ functions/main.py
@@ -0,0 +1,17 @@
+# Welcome to Cloud Functions for Firebase for Python!
+# To get started, simply uncomment the below code or create your own.
+# Deploy with `firebase deploy`
+
+from firebase_functions import https_fn
+from firebase_admin import initialize_app
+
+# initialize_app()
+#
+#
+# @https_fn.on_request()
+# def on_request_example(req: https_fn.Request) -> https_fn.Response:
+#     return https_fn.Response("Hello world!")
+
+@identity_fn.before_user_created()
+def created_noop(event: identity_fn.AuthBlockingEvent) -> identity_fn.BeforeCreateResponse | None:
+    return
